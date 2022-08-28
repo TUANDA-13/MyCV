2356aabe5e86e166af6b9acf2e53248ce934ec18
TUANDA-13 <duonganhtuan1314@gmail.com>
2022-08-28 18:39:27 +0700

    Add Config Heroku

--- MyCV/settings.py
+++ MyCV/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'duonganhtuanbkdn@gmail.com'
 EMAIL_HOST_PASSWORD = 'evznuclndfqodmnj'
+
+django_heroku.settings(locals())
